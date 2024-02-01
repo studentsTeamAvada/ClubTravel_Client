c091335b196d9e378b6e72734b7446a97a23a78b
Alex <alexandre.kovalchuk@gmail.com>
2024-02-01 13:51:59 +0200

    Updates

--- assets/Hotel-My8fOu--.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-9p4a8sJU.js";import{b as o}from"./swiper--c4fLOBO.js";o();
--- assets/Text_page-o1wNzqAS.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-9p4a8sJU.js";import{H as r}from"./header-msV6j5kN.js";new r;
