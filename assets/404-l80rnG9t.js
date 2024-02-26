05c8f5c0b993e0a4016cbc1d1086e6a75e14622c
artem <mr_artem>
2024-02-26 23:38:24 +0200

    Updates

---
+++ assets/404-l80rnG9t.js
@@ -0,0 +1,1 @@
+import{H as e}from"./header-30OnrdDG.js";document.addEventListener("DOMContentLoaded",()=>{new e});
--- assets/Text_page-xkIBQuvS.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./header-Eu9nKmC_.js";document.addEventListener("DOMContentLoaded",()=>{new e});
